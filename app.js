1c1dcb00dc56d46f3e5c7aea4e2a23c81ce4bde5
Matthew Hoang <hoangmatthew1999@gmail.com>
2020-06-10 22:19:06 -0700

    got the form request to work

--- app.js
+++ app.js
@@ -5,10 +5,22 @@ const users = [];
 
 app.use( bodyParser.json() );
 app.use( express.static('html') );
+let urlEncoded = bodyParser.urlencoded({ extended: true });
+
 app.get('/item/:item', (req,res) =>{
     let item = req.params['item'];
     return res.send(item);
 } );
+app.post('/practice', urlEncoded,(req,res) =>{
+    // res.send(`Full name is:${req.fname} ${req.lname}.`);
+    console.log(req.body);
+    // let params = req.body.fname;
+    // console.log(params);
+    let input = req.body.fname
+    return res.redirect(`/item/${input}`);
+    return res.send(params);
+
+} );
 
 
-app.listen(3005,function(){console.log("app is running");});
+app.listen(3008,function(){console.log("app is running");});
